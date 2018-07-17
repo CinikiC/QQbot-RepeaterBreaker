974c6fd0194f07593cb1d7619fa23f0888afbbdf
CinikiC <41052385+CinikiC@users.noreply.github.com>
2018-07-17 22:59:44 +0800

    Add files via upload

---
+++ Newbe.Mahua.Plugins.RepeaterBreaker/MahuaEvents/GroupMessageReceivedMahuaEvent.cs
@@ -0,0 +1,83 @@
+﻿using Newbe.Mahua.MahuaEvents;
+using System;
+using System.Linq;
+
+namespace Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents
+{
+    public static class Common
+    {
+        //存储全局静态变量
+        public static int repeatTime = 0;   //复读次数，初值为0
+        public static string msg = "000";   //截获群消息用于复读行为检测
+        public static int repeatExecuate = 5;   //检测复读次数，可通过私戳消息自定义
+        public static System.Collections.Generic.List<string> repeaterList = new System.Collections.Generic.List<string>();   //参与复读的复读机名单
+        public static int execuateTime = 1;   //禁言时间
+        public static int execuateMode = 0;   //执行模式
+    }
+    /// <summary>
+    /// 群消息接收事件
+    /// </summary>
+    public class GroupMessageReceivedMahuaEvent
+        : IGroupMessageReceivedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+
+        public GroupMessageReceivedMahuaEvent(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessGroupMessage(GroupMessageReceivedContext context)
+        {
+            
+            //throw new NotImplementedException();
+
+            //复读计数器计数，连续两个消息内容相同，计数器+1并将发言的群成员加入复读机成员名单，反之重置计数器和名单
+            if (Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.msg == context.Message)
+            {
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeatTime++;
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList.Add(context.FromQq);
+            }
+            else
+            {
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.msg = context.Message;
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList.Clear();
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeatTime = 0;
+            }
+
+            //复读事件触发，根据执行模式进行禁言操作
+            if (Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeatTime == Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeatExecuate)
+            {
+                //正常禁言：禁言最后一个发言的复读机
+                if (Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateMode==0)
+                {
+                    _mahuaApi.BanGroupMember(context.FromGroup, context.FromQq, TimeSpan.FromMinutes(Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateTime));
+                }
+
+                //随机禁言：从复读机名单中随机选择一个禁言
+                if (Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateMode == 1)
+                {
+                    Random ran = new Random();
+                    int RandKey = ran.Next(0, Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList.Count()-1);
+                    _mahuaApi.BanGroupMember(context.FromGroup, Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList[RandKey], TimeSpan.FromMinutes(Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateTime));
+                }
+
+                //强力禁言：禁言在复读机名单中的额所有人
+                if(Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateMode==2)
+                {
+                    for(int k=0; k<=Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList.Count; k++)
+                    {
+                        _mahuaApi.BanGroupMember(context.FromGroup, Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList[k], TimeSpan.FromMinutes(Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateTime));
+                    }
+                }
+
+                //重置计数器和复读机名单
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeatTime = 0;
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeaterList.Clear();
+            }
+           
+            // 不要忘记在MahuaModule中注册
+            }
+    }
+}
---
+++ Newbe.Mahua.Plugins.RepeaterBreaker/MahuaEvents/PrivateMessageFromFriendReceivedMahuaEvent1.cs
@@ -0,0 +1,111 @@
+﻿using Newbe.Mahua.MahuaEvents;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents
+{
+    /// <summary>
+    /// 来自好友的私聊消息接收事件
+    /// </summary>
+    public class PrivateMessageFromFriendReceivedMahuaEvent1
+        : IPrivateMessageFromFriendReceivedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+
+        //getDigit()函数从字符串中截取出数字并转换成整型
+        public int getDigit(string str)
+        {
+            Regex r = new Regex("\\d+\\.?\\d*");    //设置匹配用的正则表达式
+            bool isMatch = r.IsMatch(str);
+            MatchCollection mc = r.Matches(str);
+            string result = string.Empty;   
+
+            //生成处理后的字符串，应该只有数字部分
+            for(int i=0; i<mc.Count; i++)
+            {
+                result += mc[i];
+            }
+            
+            return int.Parse(result);   //数字字符串转换为整型数
+        }
+
+        public PrivateMessageFromFriendReceivedMahuaEvent1(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessFriendMessage(PrivateMessageFromFriendReceivedContext context)
+        {
+            // todo 填充处理逻辑
+            //throw new NotImplementedException();
+
+            //功能介绍
+            _mahuaApi.SendPrivateMessage(context.FromQq)
+                .Text("通过和Repeater Breaker好友聊天，可以设置Repeater Breaker的运行模式")
+                .Newline()
+                .Text("使用以下命令，可以更改相应的运行特性")
+                .Newline()
+                .Text("设置复读检测次数 repeatTime=X X是3-10内的整数，是触发禁言所需的复读次数")
+                .Newline()
+                .Text("设置禁言时间 banTime=X X代表禁言时间，单位分钟")
+                .Newline()
+                .Text("调整执行模式 modeSet=X X取0、1、2，分别代表只禁言最后一个复读机，随机禁言一个复读机和禁言所有的复读机")
+                .Done();
+
+            //匹配命令类型的正则表达式
+            Regex counter = new Regex("repeatTime");
+            Regex ban = new Regex("banTime");
+            Regex mode = new Regex("modeSet");
+
+            //根据不同的输入命令调整相应的参数值
+            if(counter.IsMatch(context.Message)==true)
+            {
+                int result = getDigit(context.Message);
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.repeatExecuate = result;
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("复读次数检测阈值已调整为 ")
+                    .Text(Convert.ToString(result))
+                    .Done();
+            }
+            
+            if(ban.IsMatch(context.Message)==true)
+            {
+                int result = getDigit(context.Message);
+                Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateTime = result;
+                _mahuaApi.SendPrivateMessage(context.FromQq)
+                    .Text("禁言时间已调整为 ")
+                    .Text(Convert.ToString(result))
+                    .Done();
+            }
+
+            if(mode.IsMatch(context.Message)==true)
+            {
+                int result = getDigit(context.Message);
+                if(result<0||result>2)
+                {
+                    _mahuaApi.SendPrivateMessage(context.FromQq)
+                        .Text("设定执行模式错误：模式值只能为0，1，2中的一个！")
+                        .Done();
+                }
+                else
+                {
+                    Newbe.Mahua.Plugins.RepeaterBreaker.MahuaEvents.Common.execuateMode = result;
+                    switch(result)
+                    {
+                        case 0: _mahuaApi.SendPrivateMessage(context.FromQq)
+                                    .Text("执行模式已设置为 常规模式，只禁言最后一个复读机")
+                                    .Done();break;
+                        case 1: _mahuaApi.SendPrivateMessage(context.FromQq)
+                                    .Text("执行模式已设置为 随机模式，随机禁言一个复读机")
+                                    .Done();break;
+                        case 2: _mahuaApi.SendPrivateMessage(context.FromQq)
+                                    .Text("执行模式已设置为 强力模式，禁言全部复读机")
+                                    .Done();break;
+                    }
+                }
+            }
+            // 不要忘记在MahuaModule中注册
+        }
+    }
+}
